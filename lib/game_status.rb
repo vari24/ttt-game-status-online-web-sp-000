0c3217d41b79c4453773c894423cb55cf91e7519
Sara Varisara <varisara.n@gmail.com>
2018-12-31 05:49:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,9 +23,3 @@ WIN_COMBINATIONS = [[0,1,2],
 [0,4,8],
 [6,4,2]]
 
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-
-top_row_win = [0,1,2]
-if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
-  "X won in the top row"
-end
